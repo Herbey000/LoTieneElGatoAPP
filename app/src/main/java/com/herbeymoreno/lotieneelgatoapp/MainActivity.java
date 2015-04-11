ec09dd5b06fd59d2489565587a02179e9d1c99bc
Herbey000 <herbey.moreno@hotmail.com>
2015-04-11 16:56:08 -0700

    Update
    Share audio

--- app/src/main/java/com/herbeymoreno/lotieneelgatoapp/MainActivity.java
+++ app/src/main/java/com/herbeymoreno/lotieneelgatoapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.herbeymoreno.lotieneelgatoapp;
+
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.*;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.media.AudioManager;
 import android.content.Intent;
-import android.appwidget.AppWidgetManager;
-import android.widget.RelativeLayout;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -32,28 +36,45 @@ SoundManager sound;
 
         Intent intent = getIntent();
 
-        if(intent!=null)
-        {
-            playSoundS();
+        final int[] mSongs = new int[] { R.raw.huron, R.raw.carisaurio, R.raw.gato,R.raw.santino,R.raw.pinchigato };
+        for (int i = 0; i < mSongs.length; i++) {
+            try {
+                String path = Environment.getExternalStorageDirectory() + "/LoTieneElGatoSounds";
+                File dir = new File(path);
+                if (dir.mkdirs() || dir.isDirectory()) {
+                    String str_song_name = i + ".ogg";
+                    CopyRAWtoSDCard(mSongs[i], path + File.separator + str_song_name);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
 
 
     }
-    public void  playSoundS(){
-        sound.play(Huron);
-        }
+    public void ShareSound(View view){
+        String path = Environment.getExternalStorageDirectory() + "/LoTieneElGatoSounds";
+        File audio = new File(path+"/0.ogg");
+        Intent intent = new Intent(Intent.ACTION_SEND).setType("audio/*");
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(audio));
+        startActivity(Intent.createChooser(intent, "LoTieneElGatoAPP"));
+    }
+
 
     public void  playSound(View view){
 
         int id = view.getId();
         switch (id)
         {
-            case R.id.btnGato:
-                sound.play(Gato);
+            case R.id.btnHuron:
+                sound.play(Huron);
                 break;
             case R.id.btnCarisaurio:
                 sound.play(Carisaurio);
+                break;
+            case R.id.btnGato:
+                sound.play(Gato);
                 break;
             case R.id.btnSantino:
                 sound.play(Santino);
@@ -61,8 +82,8 @@ SoundManager sound;
             case R.id.btnPinchiGato:
                 sound.play(PinchiGato);
                 break;
-            case R.id.btnHuron:
-                sound.play(Huron);
+            case R.id.btnShare:
+                ShareSound(view);
                 break;
         }
     }
@@ -97,4 +118,21 @@ SoundManager sound;
 
         return super.onOptionsItemSelected(item);
     }
+
+      private void CopyRAWtoSDCard(int id, String path) throws IOException {
+          InputStream in = getResources().openRawResource(id);
+          FileOutputStream out = new FileOutputStream(path);
+          byte[] buff = new byte[1024];
+          int read = 0;
+          try {
+              while ((read = in.read(buff)) > 0) {
+                  out.write(buff, 0, read);
+              }
+          } finally {
+              in.close();
+              out.close();
+          }
+      }
+
+
 }
